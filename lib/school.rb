0eabd5f71d20fc9b151dbb72817209c3f8e719bd
Emily Zhao <tong.zhao.92@gmail.com>
2017-10-26 22:25:07 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
 # code here!
+require 'pry'
+class School
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+
+
+  def roster
+    @roster
+  end
+
+  def add_student (student,grade)
+    if @roster.keys.include?(grade)
+      @roster[grade]<<student
+    else
+      @roster[grade] = []
+      @roster[grade]<<student
+    end
+  end
+
+  def grade(grade_number)
+    @roster[grade_number]
+  end
+
+  def sort
+    
+end
